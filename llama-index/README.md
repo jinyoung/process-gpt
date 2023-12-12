d03b4fecb8f46262ccbb69abacac8e34b7a5a2fb
Jinyoung Jang <jyjang@uengine.org>
2023-12-12 11:11:10 +0000

    add llamaindex for semantic document search

---
+++ llama-index/README.md
@@ -0,0 +1,7 @@
+pip install llama-index
+pip install python-pptx
+pip install transformers
+pip install Pillow
+pip install torch
+pip install torchvision
+python starter.py
---
+++ llama-index/starter.py
@@ -0,0 +1,40 @@
+import os.path
+from llama_index import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+from pathlib import Path
+from llama_index import download_loader
+
+# reader = download_loader("PptxReader")
+reader = download_loader("PDFReader")
+
+
+loader = PptxReader()
+
+# check if storage already exists
+if not os.path.exists("./storage"):
+    # load the documents and create the index
+    #documents = SimpleDirectoryReader("data").load_data()
+    #documents = loader.load_data(file=Path('./data/ppt.pptx'))
+    documents = loader.load_data(file=Path('./data/pdf.pdf'))
+
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist()
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)
+
+
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What's the file name in the data directory?")
+print(response)
+
+response = query_engine.query("DXez 가 뭐야?")
+print(response)
